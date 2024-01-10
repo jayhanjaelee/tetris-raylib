11f795f0df8e90dabc5d6e15ad47bb283bc04b72
jayhanjaelee <jayhanjaelee@gmail.com>
2024-01-10 13:27:48 +0900

    Add Game Window.

---
+++ tetris-raylib/Game.cpp
@@ -0,0 +1,37 @@
+#include "Game.h"
+#include <raylib.h>
+#include <assert.h>
+
+Game::Game(int width, int height, std::string title)
+{
+    assert(GetWindowHandle()); // If assertion triggers : Window is already opened.
+    InitWindow(width, height, title.c_str());
+}
+
+Game::~Game() noexcept
+{
+    assert(GetWindowHandle()); // If assertion triggers : Window is already closed.
+    CloseWindow();
+}
+
+bool Game::GameShouldClose() const
+{
+    return false;
+}
+
+void Game::Tick()
+{
+    BeginDrawing();
+    Update();
+    Draw();
+    EndDrawing();
+}
+
+void Game::Draw()
+{
+    ClearBackground(RAYWHITE);
+}
+
+void Game::Update()
+{
+}
---
+++ tetris-raylib/Game.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+class Game
+{
+public:
+    Game(int width, int height, std::string title);
+    Game(const Game& other) = delete;
+    Game& operator = (const Game& other) = delete;
+    ~Game() noexcept;
+    bool GameShouldClose() const;
+    void Tick();
+
+private:
+    void Draw();
+    void Update();
+};
